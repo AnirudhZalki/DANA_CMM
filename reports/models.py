204aca6c741cc1319bdf4c5fe743f4a4cbd2295d
AnirudhZalki <zalkianirudh@gmail.com>
2025-12-03 23:11:40 +0530

    Added gunicorn and django version change

--- reports/models.py
+++ reports/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from djongo import models
-
 class CMMReport(models.Model):
     line = models.CharField(max_length=100)
     machine = models.CharField(max_length=100)
